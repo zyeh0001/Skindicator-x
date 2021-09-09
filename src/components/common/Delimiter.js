553d1770c0dd430f9943ce6c4410d1d31be3b90d
MinuetW <rafa0730@hotmail.com>
2021-09-10 06:01:40 +1000

    delimiter component

---
+++ src/components/common/Delimiter.js
@@ -0,0 +1,14 @@
+
+function Delimiter(props) {
+    return (
+        <div className="image-wrapper">
+            <img
+              src={props.image}
+              alt=""
+              width = "100%"
+
+            />
+        </div>
+    );
+  }
+  export default Delimiter;
