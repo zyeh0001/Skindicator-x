4c5438a22bb257cd924a4943938698e2def3391a
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2021-09-10 17:47:23 +1000

    update-api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/default/model-test", {
-      target: "https://qur0qdu5dd.execute-api.ap-southeast-2.amazonaws.com",
+      target: "https://ragtq7f8dd.execute-api.us-east-1.amazonaws.com",
       changeOrigin: true,
-      pathRewrite: { "^/default/model-test": "" },
     })
   );
 };
