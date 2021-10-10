ef899ebbc34b97d934fb4d4e9e64fb26b3c132c8
MinuetW <rafa0730@hotmail.com>
2021-10-10 16:12:46 +1100

    layout change i3

--- src/pages/MedicalSupport.js
+++ src/pages/MedicalSupport.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ClinicMap from "../components/MedicalSupportCom/ClinicMap";
+import HeaderImage from "../components/Sections/HeaderImage";
+import MainTopic from "../components/Sections/MainTopic";
 
 function MedicalSupportPage() {
   return (
-    <div>
+    <div className="featureP">
+      <HeaderImage
+        image="images/medicalSupport.png"
+      />
+      <MainTopic
+        title="Clinics around you"
+      />
+      <div className="container">
       <ClinicMap />
+      </div>
+      <div className="containerPink">
+        <MainTopic
+          title="        Third-party services"
+        />
+      </div>
     </div>
   );
 }
