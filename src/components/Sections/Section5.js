baeaff2c10cac9257d6fa9fa392d3560a999bac0
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2021-10-12 03:40:14 +1100

    fix-moble mode bug-charles

--- src/components/Sections/Section5.js
+++ src/components/Sections/Section5.js
@@ -1,26 +1,23 @@
-import { Link } from "react-router-dom";
 function Section5(props) {
   return (
     <div className="col-2">
       <section>
         <div className="image-wrapper">
-          <Link to="/self-check" className="image">
-            <img
-              src={props.image}
-              alt=""
-              style={{
-                width: "200px",
-                height: "280px",
-              }}
-            />
-          </Link>
+          <img
+            src={props.image}
+            alt=""
+            style={{
+              width: "auto",
+              height: "280px",
+            }}
+          />
         </div>
         <header>
           <h3>{props.name}</h3>
         </header>
         <p
           style={{
-            width: "200px",
+            width: "auto",
           }}
         >
           {props.intro}
